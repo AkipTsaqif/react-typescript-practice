0ee0d04fb446d74d6544cb1960ebf4e149cb5ff3
AkipTsaqif <akiptsaqif@gmail.com>
2022-06-08 11:00:04 +0700

    ganti list di Todos.tsx jadi pake komponen TodoItems

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,10 +1,11 @@
 import Todo from '../models/todo';
+import TodoItem from './TodoItems';
 
 const Todos: React.FC<{todos: Todo[]}> = (props) => {
     return (
         <ul>
             {props.todos.map((todo) => (
-                <li key={todo.id}>{todo.text}</li>
+                <TodoItem key={todo.id} text={todo.text} />
             ))}
         </ul>
     )
